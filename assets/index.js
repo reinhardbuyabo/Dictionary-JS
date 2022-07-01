30738f26ff3a3b9bfc8f89ba652a0ecb6131cb50
reinhardbuyabo <buyabor@gmail.com>
2022-07-01 16:40:35 +0300

    trial run

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,16 @@
 const wrapper = document.querySelector(".wrapper"),
   searchInput = wrapper.querySelector("input"),
-  synonyms = wrapper.querySelector(".synonym .list"),
+  synonymous = wrapper.querySelector(".synonym .list"),
   volume = wrapper.querySelector(".word i"),
   removeIcon = wrapper.querySelector(".search span"),
-  infoText = wrapper.querySelector(".info-text");
+  infoText = wrapper.querySelector(".info-text"),
+
+  details = wrapper.querySelector(".word .details p"),
+  phonetics = wrapper.querySelector(".word .details span"),
+  meaning = wrapper.querySelector(".meaning span"),
+  example = wrapper.querySelector(".example span");
+
+
 let audio;
 
 // fetch api function
@@ -13,6 +20,41 @@ function fetchApi(word) {
   infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
   let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
   fetch(url)
-    .then((res) => res.json())
-    .then((result) => data(result, word));
+    .then((res) => {
+      let prom = res.json(); // reads response stream to completion and returns a promise which resolves with the result of parsing the body text as JSON
+      console.log(prom);
+      return prom;
+    }) // res is a response object returned from fetch containing the embodiment of all data we need.
+    .then((result) => data(result, word))
+    .catch(err => console.log(err));
+}
+
+searchInput.addEventListener("keyup", e => {
+    fetchApi(e.target.value);
+})
+
+data = (res, w) => {
+  if (res.resolution) {
+    infoText.innerHTML = `${res.title} for <span>"${w}</span>."`;
+  } else {
+    console.log(res);
+    wrapper.classList.add("active");
+    details.innerHTML = w;
+    meaning.innerHTML = res[0].meanings[0].definitions[0].definition;
+    phonetics.innerHTML = res[0].phonetic ?? res[0].phonetics[1].text ?? "Phonetic not found";
+    example.innerHTML = res[0].meanings[0].definitions[0].example ?? "No example found";
+    console.log(res[0].meanings[0].synonyms);
+    let listofsyn = res[0].meanings[0].synonyms;
+    synonymous.innerHTML = " ";
+    if (listofsyn.length == 0){
+      synonymous.innerHTML = "<span>No synonyms found.</span>";
+    } else {
+      let output = listofsyn.map(synonym => {
+        let sp = document.createElement('span');
+        sp.textContent = synonym;
+        return sp;
+      });
+      synonymous.append(...output);
+    }
+  }
 }
